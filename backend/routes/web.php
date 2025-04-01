68af4aa4f502d82584ec93e4a4d256eecb67ebfc
Joao Batista <joaobatistajbmj@gmail.com>
2025-04-01 01:50:47 -0300

    feat(backend): Implementando autenticação com passport

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(function ($router) {
+    $router->forAccessToken();
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
