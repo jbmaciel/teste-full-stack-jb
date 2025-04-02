dfccf4bb5bb56a53e42cacefc97d3a22979d9d74
jbmaciel <joaobatistajbmj@gmail.com>
2025-04-02 08:50:21 -0300

    feat(frontend) Implementando layout base

--- frontend/src/app/components/layout-base/layout-base.component.ts
+++ frontend/src/app/components/layout-base/layout-base.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout-base',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './layout-base.component.html',
   styleUrl: './layout-base.component.css'
 })
 export class LayoutBaseComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
