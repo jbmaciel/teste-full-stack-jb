5b5e17e7b1f3d0884adae36553da87ff56e00d96
jbmaciel <joaobatistajbmj@gmail.com>
2025-04-02 21:24:36 -0300

    feat(frontend) Criação do Form para Entidades

---
+++ frontend/src/app/components/entidades/entidades-form/entidades-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entidades-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './entidades-form.component.html',
+  styleUrl: './entidades-form.component.css'
+})
+export class EntidadesFormComponent {
+
+}
