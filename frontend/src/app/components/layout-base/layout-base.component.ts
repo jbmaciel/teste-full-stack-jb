a7ac37063b45eabaf96bd3e94b23163d3cabf087
jbmaciel <joaobatistajbmj@gmail.com>
2025-04-03 20:20:05 -0300

    feat(frontend) add navbar

--- frontend/src/app/components/layout-base/layout-base.component.ts
+++ frontend/src/app/components/layout-base/layout-base.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout-base',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './layout-base.component.html',
   styleUrl: './layout-base.component.css'
 })
 export class LayoutBaseComponent {
+  user: any;
   constructor(private authService: AuthService, private router: Router) {}
 
+  ngOnInit(): void {
+      this.authService.user$.subscribe(user => {
+          this.user = user;
+      });
+  }
+
+  
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
   }
+
 }
