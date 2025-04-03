dfb0c153208b2ea9f7390d2934339bc10e806b42
jbmaciel <joaobatistajbmj@gmail.com>
2025-04-02 21:02:57 -0300

    fix(frontend) Apenas renomeando rotas "entidades"

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,20 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
+import { LayoutBaseComponent } from './components/layout-base/layout-base.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutBaseComponent,
+    canActivate: [AuthGuard], //Protegendo as rotas
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      // Lazy loading de módulos
+      { path: 'entidades', loadChildren: () => import('./modules/entidades/entidades.module').then(m => m.EntidadesModule) }
+
+    ],
+  },
+  { path: '**', redirectTo: '/login' },
 ];
